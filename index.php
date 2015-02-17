cae8521d388d687542969b82da45a968eedb808e
Karlis Baumanis <karl.baumann@live.de>
2015-02-17 13:40:26 +0000

    added bootstrap

--- index.php
+++ index.php
@@ -1,52 +1,20 @@
-<!DOCTYPE HTML>
-
-<html>
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title>Your Website</title>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </head>
-
 <body>
 
-  <header>
-    <nav>
-      <form action='http://dunb.lv/submit.php' method=GET>
-        <input type=text name=kaka value=blaablaa id=kaka/>
-        <button type=submit>Poga</button>
-      </form>
-    </nav>
-  </header>
-
-  <section>
-
-    <article>
-      <header>
-        <h2>Article title</h2>
-        <p>Posted on <time datetime="2009-09-04T16:31:24+02:00">September 4th 2009</time> by <a href="#">Writer</a> - <a href="#comments">6 comments</a></p>
-      </header>
-      <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-    </article>
-
-    <article>
-      <header>
-        <h2>Article title</h2>
-        <p>Posted on <time datetime="2009-09-04T16:31:24+02:00">September 4th 2009</time> by <a href="#">Writer</a> - <a href="#comments">6 comments</a></p>
-      </header>
-      <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-    </article>
-
-  </section>
-
-  <aside>
-    <h2>About section</h2>
-    <p>Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-  </aside>
-
-  <footer>
-    <p>Copyright 2009 Your name</p>
-  </footer>
+<div class="container-fluid">
+    <h1>My First Bootstrap Page</h1>
+    <p>This is some text.</p>
+</div>
 
 </body>
-
 </html>
+</div>
